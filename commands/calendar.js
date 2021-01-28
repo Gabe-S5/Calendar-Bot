0f0670541290f4b95ef1ad26d19a520109dae92f
Gabe-S5 <gabrielsalisi15@gmail.com>
2021-01-28 16:32:37 -0500

    remove addEvent

--- commands/calendar.js
+++ commands/calendar.js
@@ -1,6 +1,4 @@
 const Discord = require("discord.js");
-const clear = require("./clear");
-
 const monthNames = ["January", "February", "March", "April", "May", "June",
 "July", "August", "September", "October", "November", "December"];
 const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -21,7 +19,7 @@ function getDaysOfWeek(day) {
 }
 function createEmbed() {
     const fullDates = getDaysOfWeek(day)
-    const modelEmbed = new Discord.MessageEmbed()
+    var modelEmbed = new Discord.MessageEmbed()
     .setColor('#0000FF')
     .setTitle('My Calender for the Week! ')
     .setDescription('The following are my goals and tasks for the week. ')
@@ -70,72 +68,9 @@ function createEmbed() {
 
 const newEmbed = new Discord.MessageEmbed(createEmbed())
 
-function addEvent(message) {
-    const prompts = [
-        'Which date would you like to modify? ',
-        'Input text you would like to write: '
-    ]
-    let counter = 0
-
-    const filter = m => m.author.id === message.author.id
-    
-    const collector = new Discord.MessageCollector(message.channel, filter, {
-        max: prompts.length,
-        time: 1000 * 15
-    })
-
-    message.channel.send(prompts[counter++])
-    collector.on('collect', m => {
-        if (counter < prompts.length) {
-            m.channel.send(prompts[counter++])
-        }
-    })
-
-    collector.on('end', collected => {
-        console.log(`Collected ${collected.size} messages`)
-
-        if (collected.size < prompts.length) {
-            message.reply('You did not reply in time. ')
-            return
-        }
-        let counter = 0
-        let match = false
-        let matchDate = '', newEvent = ''
-        collected.forEach((value) => {
-            console.log(prompts[counter], value.content)
-            for (let field of newEmbed.fields) {
-                if (field.name.split(', ')[0].toLowerCase() === value.content.toLowerCase()) {
-                    match = true
-                    break
-                }
-            }               
-            if (match) {
-                if (counter === 0) matchDate = value.content
-                else newEvent = value.content
-            }
-            counter++
-        })
-        if (match) {
-            for (field of newEmbed.fields) {
-                if (field.name.split(', ')[0].toLowerCase() === matchDate.toLowerCase()) {
-                    field.value = newEvent
-                    break
-                }
-            }
-            clearPrevious(message, 5)
-            message.channel.send(newEmbed)
-        }
-    })
-}
-function clearPrevious(message, amount) {
-    message.channel.messages.fetch({ limit: amount }).then(messages => {
-        message.channel.bulkDelete(messages, true);
-    })
-}
-
 module.exports = {
     name: 'calendar',
-    description: 'Shows current week with any events or notes that may have been added. ',
+    description: 'Shows your current calendar. Create new calendar with !calendar new.  ',
     execute(message, args) {
         if (!args[0]) {
             message.channel.send('Please indicate whether you would like to view the current calendar, add from the calendar or make a new calendar. (view, add, new) ')
@@ -143,12 +78,19 @@ module.exports = {
         else if (args[0] === 'view') {
             message.channel.send(newEmbed)
         }
-        else if (args[0] === 'add') {
+        else if (args[0] === 'event') {
             addEvent(message)
+        }
+        else if (args[0] === 'description') {
+            addDescription(message)
         }
         else if(args[0] === 'new') {
             const newEmbed = new Discord.MessageEmbed(createEmbed())
             message.channel.send(newEmbed)
         }
-    }
+        else {
+            message.channel.send(newEmbed)
+        }
+    },
+    newEmbed
 }
